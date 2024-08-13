3597a54afaef0ca0433b61f5ab78ff2b3d179a83
DixonDevs <danieldixon.dev@gmail.com>
2024-08-13 16:06:21 -0500

    latest push

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <div class="sticky-header-wrapper">
     <div class="sticky-header">
-        <img class="daniel-image" src="/8bit-image-of-Daniel.jpg" alt="8-bit Character of Daniel Dixon" width="100"
-            height="100">
+        <img class="daniel-image" src="/images/8bit-image-of-Daniel.jpg" alt="8-bit Character of Daniel Dixon"
+            width="100" height="100">
         <h1 class="name-logo">DanielDixon.dev</h1>
         <ul class="nav-links">
             <li><a href="{{ route('home') }}">HOME</a></li>
